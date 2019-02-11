eda714e84332ef220b56b7fc37187a4e1d2f3867
demiacle <demiacle@gmail.com>
2019-02-10 23:45:52 -0800

    Build 3

---
+++ precache-manifest.a43df3f777cdcc868c01137e57febff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "60490cec3d711b984f51",
+    "url": "/static/js/main.60490cec.chunk.js"
+  },
+  {
+    "revision": "c034407c72e99bf4ea1f",
+    "url": "/static/js/1.c034407c.chunk.js"
+  },
+  {
+    "revision": "60490cec3d711b984f51",
+    "url": "/static/css/main.eb255adb.chunk.css"
+  },
+  {
+    "revision": "c034407c72e99bf4ea1f",
+    "url": "/static/css/1.51260432.chunk.css"
+  },
+  {
+    "revision": "c35e71ab3d585f4bc77654f6ca488729",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.747eb9dcfebd7acbbaba2398d4167987.js"
+  "/precache-manifest.a43df3f777cdcc868c01137e57febff7.js"
 );
 
 workbox.clientsClaim();
